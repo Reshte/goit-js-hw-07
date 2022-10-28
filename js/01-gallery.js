65de7b80c56a86e5cafbb202b902a1b6a1461c99
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-28 14:25:27 +0300

    comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
-const result = _.add(2, 3);
-console.log(result);
+console.log(galleryItems);
