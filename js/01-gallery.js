d3bec8ea15bcc5d1571b1571639c7c486b8b3e55
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-28 19:24:56 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+divEl.insertAdjacentHTML("beforeend", markup);
+
+divEl.addEventListener("click", onImgClick);
+let modalImg;
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  let urlImg = evt.target.dataset.source;
+  modalImg = basicLightbox.create(`
+    <img class="modal__image" src="${urlImg}" width="800" height="600">
+`);
+  modalImg.show();
+}
+
+// modalImgEl.addEventListener("keydown", onImgEscape);
+
+// function onImgEscape(evt) {}
+
+// modalImg.close()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join("");
+
+divEl.insertAdjacentHTML("beforeend", markup);
